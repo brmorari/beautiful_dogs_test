2e1f3d2e25bccf71946e2ba20c110e1ce413cb17
Bruno A. Morari <bruno.morari@webmotors.com.br>
2026-08-21 18:55:06 -0300

    Complete base app

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,23 @@ class MainApp extends StatefulWidget {
   State<MainApp> createState() => _MainAppState();
 }
 
-String? dogImage;
-
 class _MainAppState extends State<MainApp> {
+  String? dogImage;
+  bool? isLoading;
+
   Future<void> _atualizarImagem() async {
+    setState(() {
+      isLoading = true;
+    });
     final novaImagem = await getDog();
     if (novaImagem != null) {
       setState(() {
         dogImage = novaImagem;
+        isLoading = false;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
       });
     }
   }
@@ -30,22 +39,62 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 241, 239, 239),
         body: SizedBox(
           width: double.infinity,
           child: Column(
             children: [
+              SizedBox(height: 100),
+              Text(
+                'Random Dog!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight(550)),
+              ),
               Expanded(
                 child: Center(
                   child: dogImage == null
-                      ? const Text('Clique no botão para gerar um cachorro')
+                      ? Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.pets),
+                            SizedBox(height: 10),
+                            const Text(
+                              'Clique no botão para gerar seu primeiro doguito.',
+                            ),
+                          ],
+                        )
                       : Image.network(dogImage!),
                 ),
               ),
               ElevatedButton(
-                onPressed: _atualizarImagem,
-                child: Text('Gerar Doguito'),
+                onPressed: isLoading == true ? null : _atualizarImagem,
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(15),
+                  backgroundColor: isLoading == true
+                      ? Colors.grey.shade400
+                      : Colors.blueAccent,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(10),
+                  ),
+                ),
+                child: isLoading == true
+                    ? const SizedBox(
+                        height: 20,
+                        width: 88,
+                        child: Center(
+                          child: SizedBox(
+                            width: 20,
+                            height: 20,
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      )
+                    : Text('Gerar Doguito'),
               ),
-              SizedBox(height: 50),
+              SizedBox(height: 80),
             ],
           ),
         ),
